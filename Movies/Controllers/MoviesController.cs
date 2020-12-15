d7ab50f8b5ca69ec773f36094e2b9545cf0bb4e7
Andrey Emelyanov <56556713+reshxs@users.noreply.github.com>
2020-12-15 13:33:52 +0400

    Client Initial

--- Movies/Controllers/MoviesController.cs
+++ Movies/Controllers/MoviesController.cs
@@ -37,7 +37,7 @@ namespace Movies.Controllers
         public async Task<ActionResult<IEnumerable<ListMovie>>> GetMoviesByDate()
         {
             return await _context.Movies
-                .OrderBy(m => m.PublishDate)
+                .OrderByDescending(m => m.PublishDate)
                 .Select(m => m.ToListMovie())
                 .ToListAsync();
         }
